dfb8218dd173f9793af5ceb924cd54ba6717e6b3
SambaSivaRao18 <sambayadav121@gmail.com>
2026-01-21 23:29:30 +0530

    add internship

--- assets/script.js
+++ assets/script.js
@@ -193,3 +193,15 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('contextmenu', function (e) {
   e.preventDefault();
 });
+
+document.addEventListener("keydown", function (e) {
+  if (
+    e.ctrlKey &&
+    (e.key === "u" || e.key === "U" ||
+     e.key === "s" || e.key === "S" ||
+     e.key === "i" || e.key === "I")
+  ) {
+    e.preventDefault();
+  }
+});
+
